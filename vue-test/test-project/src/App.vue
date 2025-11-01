268034cef4b302a014362127b3cc8e21e4c3952f
XinR <148018754+gaiyyyy@users.noreply.github.com>
2025-11-01 14:32:41 +0800

    Add files via upload

---
+++ vue-test/test-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <user-view></user-view> -->
+    <!-- <question-view></question-view> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import UserView from './views/elment/UserView.vue'
+// import QuestionView from './views/elment/QuestionView.vue'
+
+export default {
+  // components: {
+  //   // UserView,
+  //   QuestionView
+  // },
+  data(){
+    return {
+      message: 'Hello Vue!',
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+
+
+<style>
+
+</style>
